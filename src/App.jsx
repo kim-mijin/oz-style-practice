57c8176a08cb65b6825c713e0ccd66773559eb70
kim-mijin <mycrimosn@naver.com>
2025-07-10 15:10:38 +0900

    💄 💄 Styled-Components 적용
    CSS 모듈화 + flexMixin, fontMixin 등 스타일 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import "./App.scss";
+import styled from "styled-components";
+// import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { flexMixin } from "./style/style";
+
+
+const StyledSection = styled.section`
+${flexMixin({ justify: "center", align: "center", wrap: "wrap", gap: 20})}
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
-    <main>
+    <>
       <Header />
-      <section>
+      <StyledSection>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
-    </main>
+      </StyledSection>
+    </>
   );
 }
 
